478a134cfe69f939794ce9c4b1cd19682f350acd
zebing <1270105911@qq.com>
2020-10-26 07:05:01 +0800

    feat: 项目初始化

---
+++ examples/base/nuxt.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  css: [
+    // 项目里要用的 CSS 文件
+    '../../dist/scroll-mobile.css',
+  ]
+}
---
+++ examples/base/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <scroll-mobile>
+      <div class="list-item" v-for="(value, key) in list" :key="key">{{value}}</div>
+    </scroll-mobile>
+  </div>
+</template>
+<script>
+import ScrollMobile from "../../../dist";
+export default {
+  components: { ScrollMobile },
+  data() {
+    return {
+      list: [],
+    };
+  },
+  mounted () {
+    this.list = Array.from({ length: 30 }, (value, key) => key)
+  }
+};
+</script>
+<style scoped>
+.list-item {
+  font-size: 30px;
+  padding: 20px;
+  border-bottom: 1px solid;
+}
+</style>
